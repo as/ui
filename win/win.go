83c48a82ed96381f38b36ba98d44e6ef1bf991f3
as <as.utf8@gmail.com>
2017-10-13 15:01:04 -0700

    negative wg counter: revert; fixes as/a#96

--- win/win.go
+++ win/win.go
@@ -258,11 +258,8 @@ func (w *Win) filldebug() {
 func (w *Win) Refresh() {
 	w.Frame.Refresh()
 	w.UserFunc(w)
-	w.wg.Add(1)
-	w.workc <- w.b.Bounds()
-	//w.Window().Upload(w.Sp, w.b, w.b.Bounds())
+	w.Window().Upload(w.Sp, w.b, w.b.Bounds())
 	w.Flush()
-	w.wg.Done()
 	w.dirty = false
 }
 
