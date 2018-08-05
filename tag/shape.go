1ff1a7f5a3806b86595807e40db03ff861c17c38
as <as.utf8@gmail.com>
2018-08-04 23:45:52 -0700

    tag: shape: fix window field

--- tag/shape.go
+++ tag/shape.go
@@ -14,11 +14,8 @@ const (
 func (t *Tag) Move(pt image.Point) {
 	t.sp = pt
 	t.Win.Move(pt)
-	if t.Body == nil {
-		return
-	}
 	pt.Y += t.Win.Bounds().Dy()
-	t.Body.Move(pt)
+	t.Window.Move(pt)
 }
 
 func (t *Tag) Resize(pt image.Point) {
@@ -27,7 +24,7 @@ func (t *Tag) Resize(pt image.Point) {
 		pt.Y = 0
 		t.size = pt
 		t.Win.Resize(pt)
-		t.Body.Resize(pt)
+		t.Window.Resize(pt)
 		t.Vis = VisNone
 		return
 	}
@@ -43,14 +40,14 @@ func (t *Tag) Resize(pt image.Point) {
 		t.align()
 
 		pt.Y = 0
-		t.Body.Resize(pt)
+		t.Window.Resize(pt)
 		t.Vis = VisTag
 		return
 	}
 	t.size = pt
 	t.Win.Resize(image.Pt(pt.X, ts))
 	t.align()
-	t.Body.Resize(image.Pt(pt.X, pt.Y-ts))
+	t.Window.Resize(image.Pt(pt.X, pt.Y-ts))
 	t.Vis = VisFull
 }
 
@@ -58,7 +55,7 @@ func (t *Tag) align() {
 	// Coherence: window always under tag
 	r := t.Win.Bounds()
 	r.Min.Y = r.Max.Y
-	t.Body.Move(r.Min)
+	t.Window.Move(r.Min)
 }
 
 func (t *Tag) Bounds() image.Rectangle {
