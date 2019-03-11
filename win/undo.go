ea46b9c8c10f81065b22b0eb2a8e8e8f10c195ba
as <as.utf8@gmail.com>
2019-03-11 00:31:30 -0700

    win: comment for undo var

--- win/undo.go
+++ win/undo.go
@@ -1,3 +1,7 @@
 package win
 
+// EnableUndoExperiment toggles the state of the Undo/Redo
+// features. This is set to off by default. Eventually the variable
+// will be removed.
+//
 var EnableUndoExperiment = false
